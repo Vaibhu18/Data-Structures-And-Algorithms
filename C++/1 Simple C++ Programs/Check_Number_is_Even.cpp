f51287a30f9322742e30c091e82808410a0ef13e
Vaibhav Nagnath Shinde <vaibhu8605@gmail.com>
2023-01-29 00:29:54 +0530

    check even number

---
+++ C++/1 Simple C++ Programs/Check_Number_is_Even.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter any number : ";
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << num << " is a even number." << endl;
+    }
+    else
+    {
+        cout << num << " is a odd number." << endl;
+    }
+}
