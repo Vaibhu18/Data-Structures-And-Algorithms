b6af8367f756fc19d71a69331220bb245ab412c4
Vaibhav Nagnath Shinde <vaibhu8605@gmail.com>
2023-01-29 23:37:45 +0530

    29/01/23

---
+++ C++/1 Simple C++ Programs/Print_ASCII_Value_of_Characters.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter any character : ";
+    cin >> ch;
+
+        cout <<"ASCII Value of "<< ch << " is : "<< (int)ch;
+}
